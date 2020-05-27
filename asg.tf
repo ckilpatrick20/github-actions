c4c0693f9538946c79fca8d6886b9898eec4158a
Craig Kilpatrick <ckilpatrick@ipipeline.com>
2020-05-27 12:31:37 -0700

    fixing formatting

--- asg.tf
+++ asg.tf
@@ -1,18 +1,18 @@
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type  = "ELB"
-  min_size = var.min_size
-  max_size = var.max_size
+  target_group_arns    = [aws_lb_target_group.asg.arn]
+  health_check_type    = "ELB"
+  min_size             = var.min_size
+  max_size             = var.max_size
   tag {
-    key       = "Name"
+    key                 = "Name"
     value               = var.cluster_name
     propagate_at_launch = true
   }
   dynamic "tag" {
     for_each = {
-      for key, value in var.custom_tags:
+      for key, value in var.custom_tags :
       key => upper(value)
       if key != "Name"
     }
