a08cdde4031b9c8cb5d1608edd02d5dc3bc3eb69
Craig Kilpatrick <ckilpatrick@ipipeline.com>
2020-08-10 14:45:32 -0700

    fix: adding back encryption. was causing checkov action to fail.

--- example/terraform/directory/main.tf
+++ example/terraform/directory/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-0323c3dd2da7fb37d"
   instance_type = "t2.micro"
 
-  # root_block_device {
-  #   encrypted = true
-  # }
+  root_block_device {
+    encrypted = true
+  }
 
   tags = {
     Name        = "Test Instance"
